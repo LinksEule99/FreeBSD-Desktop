9deca970b875eb61d0cca6faae79b9cc8faa8db7
Commiemo <140261372+LinksEule99@users.noreply.github.com>
2026-03-22 21:13:51 +0000

    Add files via upload

---
+++ dwm/patch/include.h
@@ -0,0 +1,29 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+#include "bar.h"
+
+#include "bar_alpha.h"
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_status2d.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+#include "bar_systray.h"
+
+/* Other patches */
+#include "attachx.h"
+#include "autostart.h"
+#include "cyclelayouts.h"
+#include "fullscreen.h"
+#include "movestack.h"
+#include "pertag.h"
+#include "restartsig.h"
+#include "scratchpad.h"
+#include "swallow.h"
+#include "vanitygaps.h"
+#include "warp.h"
+/* Layouts */
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
